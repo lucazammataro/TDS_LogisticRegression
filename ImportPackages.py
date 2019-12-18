f6536b3eb6a00f91ed056bdd3677e318f41fc6ef
Luca Arrigo Zammataro <luca.zammataro@gmail.com>
2019-12-17 19:04:55 -0500

    Create ImportPackages.py

---
+++ ImportPackages.py
@@ -0,0 +1,8 @@
+# Import all the required Packages
+# All of them are part of SciPy.org
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import scipy.optimize as op 
+import seaborn as sns
+from __future__ import division
